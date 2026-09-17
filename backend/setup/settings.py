bd9f4fc5e33b94b1828971e25837725b32df2c1b
KueRocha <cauerocha012@gmail.com>
2026-09-17 00:05:21 -0300

    Fix arquivos estaticos

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -116,8 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://kuerocha.github.io'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
