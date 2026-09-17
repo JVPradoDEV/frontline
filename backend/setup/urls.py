d1573b059b0cfa9427576567c03a939232d3addb
KueRocha <cauerocha012@gmail.com>
2026-09-17 00:08:29 -0300

    Endpoint atualizado

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path('cadastro/', CadastroView.as_view(), name='cadastro'),
 
     #JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
