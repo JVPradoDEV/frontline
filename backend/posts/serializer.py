3b2a3ce0bfaf2daa3755387a1f64727a21c8ea1f
KueRocha <cauerocha012@gmail.com>
2026-09-19 22:53:04 -0300

    deu_like adicionado a Posts e Comentarios

--- backend/posts/serializer.py
+++ backend/posts/serializer.py
@@ -9,10 +9,11 @@ class PostSerializer(ModelSerializer):
     n_likes = serializers.SerializerMethodField()
     n_comentarios = serializers.SerializerMethodField()
     autor = UsuarioSerializer(read_only=True)
+    deu_like = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id','conteudo', 'n_likes', 'n_comentarios',  'autor', 'data_criacao']
+        fields = ['id','conteudo', 'n_likes', 'n_comentarios',  'autor', 'data_criacao', 'deu_like']
 
     def get_n_likes(self, obj):
         return obj.likes.count()
@@ -20,14 +21,25 @@ class PostSerializer(ModelSerializer):
     def get_n_comentarios(self, obj):
         return obj.comentarios.count()
 
+    def get_deu_like(self, obj):
+        return obj.likes.filter(
+            id=self.context['request'].user.id
+        ).exists()
+
 
 class ComentarioSerializer(ModelSerializer):
     n_likes = serializers.SerializerMethodField()
     autor = UsuarioSerializer(read_only=True)
+    deu_like = serializers.SerializerMethodField()
 
     class Meta:
         model = Comentario
-        fields = ['id','conteudo', 'n_likes', 'post', 'autor', 'data_criacao']
+        fields = ['id','conteudo', 'n_likes', 'post', 'autor', 'data_criacao', 'deu_like']
 
     def get_n_likes(self, obj):
         return obj.likes.count()
+
+    def get_deu_like(self, obj):
+            return obj.likes.filter(
+                id=self.context['request'].user.id
+            ).exists()
